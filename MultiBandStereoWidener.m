9d27311859b1290a4c7486051a8ed35c1ae6b96c
TsjnkDSP <tantep8@gmail.com>
2021-06-23 15:51:10 +0700

    Changed to CrossoverFilter function with adjustable slope

--- MultiBandStereoWidener.m
+++ MultiBandStereoWidener.m
@@ -12,11 +12,16 @@ classdef MultiBandStereoWidener < audioPlugin
         gainUpperMid    = 0;
         gainHigh        = 0;
     end
+    properties
+        Order = '6 dB/Oct';
+    end
     properties (Constant)
         PluginInterface = ...
             audioPluginInterface(...
             'InputChannels',2,...
             'OutputChannels',2,...
+            audioPluginParameter('Order',...
+            'Mapping',{'enum', '6 dB/Oct', '12 dB/Oct', '24 dB/Oct'}),...
             audioPluginParameter('gainBass',...
             'Mapping',{'lin',-20,20}),...
             audioPluginParameter('gainLowerMid',...
@@ -34,51 +39,53 @@ classdef MultiBandStereoWidener < audioPlugin
     end
  
     properties (Access = private)   
-        bB;
-        bLM;
-        bM;
-        bUM;
-        bH;
-        aB;
-        aLM;
-        aM;
-        aUM;
-        aH;
-        zB;
-        zLM;
-        zM;
-        zUM;
-        zH;
+        xFilt6
+        xFilt12
+        xFilt24
     end
     
     methods
         function obj = MultiBandStereoWidener()
             fs = getSampleRate(obj);
-            [obj.bB,obj.aB] = butter(2,88/(fs/2),'low');
-            [obj.bLM,obj.aLM] = butter(2,[88 335]/(fs/2),'bandpass');
-            [obj.bM,obj.aM] = butter(2,[355 1420]/(fs/2),'bandpass');
-            [obj.bUM,obj.aUM] = butter(2,[1420 5680]/(fs/2),'bandpass');
-            [obj.bH,obj.aH] = butter(2,5680/(fs/2),'high');
-            obj.zB = zeros(2);
-            obj.zLM = zeros(4,2);
-            obj.zM = zeros(4,2);
-            obj.zUM = zeros(4,2);
-            obj.zH= zeros(2);
+            obj.xFilt6 = crossoverFilter( ...
+                        'NumCrossovers',4, ...
+                        'CrossoverFrequencies',[88,355,1420,5680], ...
+                        'CrossoverSlopes',6, ...
+                        'SampleRate',fs);
+            obj.xFilt12 = crossoverFilter( ...
+                        'NumCrossovers',4, ...
+                        'CrossoverFrequencies',[88,355,1420,5680], ...
+                        'CrossoverSlopes',12, ...
+                        'SampleRate',fs);
+            obj.xFilt24 = crossoverFilter( ...
+                        'NumCrossovers',4, ...
+                        'CrossoverFrequencies',[88,355,1420,5680], ...
+                        'CrossoverSlopes',24, ...
+                        'SampleRate',fs);
         end
         
         function out = process(obj,in)
-            [yL,obj.zB] = filter(obj.bB,obj.aB,in,obj.zB);
-            [yLM,obj.zLM] = filter(obj.bLM,obj.aLM,in,obj.zLM);
-            [yM,obj.zM] = filter(obj.bM,obj.aM,in,obj.zM);
-            [yUM,obj.zUM] = filter(obj.bUM,obj.aUM,in,obj.zUM);
-            [yH,obj.zH] = filter(obj.bH,obj.aH,in,obj.zH);        
+            yB = zeros(size(in));
+            yLM = zeros(size(in));
+            yM = zeros(size(in));
+            yUM = zeros(size(in));
+            yH  = zeros(size(in));
+            
+            switch (obj.Order)
+                case '6 dB/Oct'
+                    [yB, yLM, yM, yUM, yH] = obj.xFilt6(in);
+                case '12 dB/Oct'
+                    [yB, yLM, yM, yUM, yH] = obj.xFilt12(in);
+                case '24 dB/Oct'
+                    [yB, yLM, yM, yUM, yH] = obj.xFilt24(in);
+            end
             
             g1 = 10^(obj.gainBass/20);     
             g2 = 10^(obj.gainLowerMid/20);   
             g3 = 10^(obj.gainMid/20);         
             g4 = 10^(obj.gainUpperMid/20);    
             g5 = 10^(obj.gainHigh/20);       
-            out = g1.*yL + g2.*yLM + g3.*yM + g4.*yUM + g5.*yH;
+            out = g1.*yB + g2.*yLM + g3.*yM + g4.*yUM + g5.*yH;
 %             % % Stereo Width
 %             mid = (in(:,1) + in(:,2)) ./ 2;
 %             side = (in(:,1) - in(:,2)) ./ 2;
@@ -94,20 +101,22 @@ classdef MultiBandStereoWidener < audioPlugin
 %             rightChannel = rightGain.*inNew(:,2);
 %             out = [leftChannel,rightChannel];
         end
+%         function set.Order(obj, val)
+%             obj.xFilt.CrossoverSlopes = val*6;%#ok<MCSUP>
+%         end
+%         function val = get.Order(obj)
+%             val = 6*obj.xFilt.CrossoverSlopes;
+%         end
         
         function reset(obj)
             fs = getSampleRate(obj);
-            [obj.bB,obj.aB] = butter(2,88/(fs/2),'low');
-            [obj.bLM,obj.aLM] = butter(2,[88 335]/(fs/2),'bandpass');
-            [obj.bM,obj.aM] = butter(2,[355 1420]/(fs/2),'bandpass');
-            [obj.bUM,obj.aUM] = butter(2,[1420 5680]/(fs/2),'bandpass');
-            [obj.bH,obj.aH] = butter(2,5680/(fs/2),'high');
-            obj.zB = zeros(2);
-
-            obj.zLM = zeros(4,2);
-            obj.zM = zeros(4,2);
-            obj.zUM = zeros(4,2);
-            obj.zH= zeros(2);
+            obj.xFilt6.SampleRate = fs;
+            reset(obj.xFilt6);
+            obj.xFilt12.SampleRate = fs;
+            reset(obj.xFilt12);
+            obj.xFilt24.SampleRate = fs;
+            reset(obj.xFilt24);
         end
     end
 end
+
